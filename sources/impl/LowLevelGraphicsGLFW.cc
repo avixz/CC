a4b6a2d0ffd6db144f76431e4bf3414db2f6ca31
avixz <avixz@3cantos.es>
2016-08-15 11:42:34 +0200

    Emscripten working! (sort of)

--- sources/impl/LowLevelGraphicsGLFW.cc
+++ sources/impl/LowLevelGraphicsGLFW.cc
@@ -38,7 +38,7 @@ namespace CC
   //---------------------------------------------------------------------------
   void LowLevelGraphicsGLFW::SetupGL()
   {
-    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
     glClear(GL_COLOR_BUFFER_BIT);
   }
 
